4e2a931603b7e8b2afc9d712782c586488eebdba
zwx <zwx@meixing.com>
2025-09-05 15:10:39 +0800

    Modify IPM.

--- ai/IPM/ipm.py
+++ ai/IPM/ipm.py
@@ -1,38 +1,48 @@
-import numpy as np
-from numpy.linalg import solve
 import os
 import sys
 import cv2
 
+import numpy as np
+from numpy.linalg import solve
+
+
 class IPM():
-    def __init__(self, args):
-        # 记录参数
-        self.raw_H, self.raw_W, self.raw_C = args.height, args.width, args.channel
-        self.vanish_line = args.vanish_line
-        param = [args.intrinsic[0], 0.0, args.intrinsic[2], 0.0, args.intrinsic[1], args.intrinsic[3], 0.0, 0.0, 1.0]
-        self.K = np.array(param).reshape(3,3)
-        self.ipm_width_x_3d, self.ipm_height_y_3d = args.ipm_width_x_3d, args.ipm_height_y_3d
-        self.ipm_step_xy = args.ipm_step_xy
-        self.ipm_height_y_start = args.ipm_height_y_start
-        self.R_ccs2ocs = np.array(args.R_ccs2ocs).reshape(3,3)
-        self.T_ccs2ocs = np.array(args.T_ccs2ocs).reshape(3,1)
+    def __init__(self, cfg):
+        # 参数
+        self.image_height, self.image_width = cfg["image_height"], cfg["image_width"]
+        self.vanish_line = cfg["vanish_line"]
+        self.world_width, self.world_height = cfg["world_width"], cfg["world_height"]
+        self.world_pixel = cfg["world_pixel"]
+        self.world_y_start = cfg["world_y_start"]
+        self.distort = np.array(cfg["distort"])
+        self.K = np.array([cfg["intrinsic"][0], 0.0, cfg["intrinsic"][2], 
+                           0.0, cfg["intrinsic"][1], cfg["intrinsic"][3], 
+                           0.0, 0.0, 1.0]).reshape(3,3)
+        self.Rwc = np.array(cfg["Rwc"]).reshape(3,3)
+        self.Twc = np.array(cfg["Twc"]).reshape(3,1)
+    
         # 读入文件夹下所有图片
-        if not os.path.exists(args.image_dir):
-            os.makedirs(args.image_dir)
-        self.image_dir = args.image_dir
-        self.images_name = sorted(os.listdir(args.image_dir))
-        # self.images_list = [os.path.join(args.image_dir, f) for f in self.images_name]
+        os.makedirs(cfg["image_dir"], exist_ok=True)
+        self.image_dir = cfg["image_dir"]
+        self.images_name = sorted(os.listdir(cfg["image_dir"]))
+
         # 输出文件路径
-        if not os.path.exists(args.output_dir):
-            os.makedirs(args.output_dir)
-        self.output_dir = args.output_dir
-        self.merge_dir = os.path.join(self.output_dir, "compare")
-        if not os.path.exists(self.merge_dir):
-            os.makedirs(self.merge_dir)
-        # 计算IPM图像大小
-        self.ipm_image_width = int(self.ipm_width_x_3d / self.ipm_step_xy)
-        self.ipm_image_height = int(self.ipm_height_y_3d / self.ipm_step_xy)
-        self.resize = args.resize
+        os.makedirs(cfg["output_dir"], exist_ok=True)
+        self.output_dir = cfg["output_dir"]
+
+        # 计算俯视图大小
+        self.ipm_image_width = int(self.world_width / self.world_pixel)
+        self.ipm_image_height = int(self.world_height / self.world_pixel)
+        self.resize = cfg["resize"]
+
+        # 计算去畸变映射
+        self.undistort_map1, self.undistort_map2 = self.get_undistort_map()
+
+        # 计算点云
+        self.point_cloud = self.get_point_cloud()
+
+        # 计算俯视图到透视图的像素映射
+        self.map, self.mask = self.get_ipm_map()
 
     def pair(self, ipm_image_width, ipm_image_height):
         for r in range(ipm_image_height):
@@ -43,20 +53,20 @@ class IPM():
         p_uv = np.append(np.array(uv),1)
         p_cn = np.linalg.inv(self.K) @ p_uv
         # AX=b
-        A00 = self.R_ccs2ocs[2,0] * p_cn[0] - self.R_ccs2ocs[0,0]
-        A01 = self.R_ccs2ocs[2,1] * p_cn[0] - self.R_ccs2ocs[0,1]
-        A10 = self.R_ccs2ocs[2,0] * p_cn[1] - self.R_ccs2ocs[1,0]
-        A11 = self.R_ccs2ocs[2,1] * p_cn[1] - self.R_ccs2ocs[1,1]
-        b0 = self.T_ccs2ocs[0] - self.T_ccs2ocs[2] * p_cn[0]
-        b1 = self.T_ccs2ocs[1] - self.T_ccs2ocs[2] * p_cn[1]
-        A = np.mat([[A00,A01],[A10,A11]])
-        b = np.mat([b0,b1])
+        A00 = self.Rwc[2,0] * p_cn[0] - self.Rwc[0,0]
+        A01 = self.Rwc[2,1] * p_cn[0] - self.Rwc[0,1]
+        A10 = self.Rwc[2,0] * p_cn[1] - self.Rwc[1,0]
+        A11 = self.Rwc[2,1] * p_cn[1] - self.Rwc[1,1]
+        b0 = self.Twc[0] - self.Twc[2] * p_cn[0]
+        b1 = self.Twc[1] - self.Twc[2] * p_cn[1]
+        A = np.asmatrix([[A00,A01],[A10,A11]])
+        b = np.asmatrix([b0,b1])
         X = solve(A,b)
 
         return X.A  # numpy.ndarray [2,1]
 
     def calc_uv_by_ccs_xy(self, pt_ccs):
-        pt_ocs = self.R_ccs2ocs @ pt_ccs + self.T_ccs2ocs.reshape(3)
+        pt_ocs = self.Rwc @ pt_ccs + self.Twc.reshape(3)
         pt_ocs = pt_ocs / pt_ocs[2]
         pt_uv = self.K @ pt_ocs
 
@@ -65,23 +75,23 @@ class IPM():
     def get_ipm_rect(self):
         # 对应960x540的投影区域
         uv_lu = [0,309]
-        uv_ru = [self.raw_W-1,309]
+        uv_ru = [self.image_width-1,309]
         uv_lb = [0,433]
-        uv_rb = [self.raw_W-1,433]
+        uv_rb = [self.image_width-1,433]
 
         ccs_lb = self.calc_ccs_xy_by_uv(uv_lb)
         ccs_rb = self.calc_ccs_xy_by_uv(uv_rb)
         try:
-            assert((-self.ipm_width_x_3d/2.0<ccs_lb[0,0]<ccs_rb[0,0]<self.ipm_width_x_3d/2.0) and (0<ccs_lb[1,0]<self.ipm_height_y_3d) and (0<ccs_rb[1,0]<self.ipm_height_y_3d))
+            assert((-self.world_width/2.0<ccs_lb[0,0]<ccs_rb[0,0]<self.world_width/2.0) and (0<ccs_lb[1,0]<self.world_height) and (0<ccs_rb[1,0]<self.world_height))
         except:
-            print("Error: ipm_width_x_3d is too big.")
+            print("Error: world_width is too big.")
             sys.exit(1)
 
         ccs_lu = self.calc_ccs_xy_by_uv(uv_lu)
         ccs_ru = self.calc_ccs_xy_by_uv(uv_ru)
         
-        if ((ccs_lu[0,0]<-self.ipm_width_x_3d/2.0) or (ccs_lu[1,0]>self.ipm_height_y_3d)):
-            ccs_lu = np.array([max(ccs_lu[0,0],-self.ipm_width_x_3d/2.0), min(ccs_lu[1,0],self.ipm_height_y_3d), 0])
+        if ((ccs_lu[0,0]<-self.world_width/2.0) or (ccs_lu[1,0]>self.world_height)):
+            ccs_lu = np.array([max(ccs_lu[0,0],-self.world_width/2.0), min(ccs_lu[1,0],self.world_height), 0])
             uv_lu = self.calc_uv_by_ccs_xy(ccs_lu).tolist()[:2]
             if (uv_lu[0]<0):
                 uv_lu[0] = 0
@@ -89,19 +99,19 @@ class IPM():
                 uv_lu[1] = self.vanish_line
             ccs_lu = self.calc_ccs_xy_by_uv(uv_lu)
 
-        if ((ccs_ru[0,0]>self.ipm_width_x_3d/2.0) or (ccs_ru[1,0]>self.ipm_height_y_3d)):
-            ccs_ru = np.array([min(ccs_ru[0,0],self.ipm_width_x_3d/2.0), min(ccs_ru[1,0],self.ipm_height_y_3d), 0])
+        if ((ccs_ru[0,0]>self.world_width/2.0) or (ccs_ru[1,0]>self.world_height)):
+            ccs_ru = np.array([min(ccs_ru[0,0],self.world_width/2.0), min(ccs_ru[1,0],self.world_height), 0])
             uv_ru = self.calc_uv_by_ccs_xy(ccs_ru).tolist()[:2]
-            if (uv_ru[0]>=self.raw_W):
-                uv_ru[0] = self.raw_W-1
+            if (uv_ru[0]>=self.image_width):
+                uv_ru[0] = self.image_width-1
             if (uv_ru[1]<self.vanish_line):
                 uv_ru[1] = self.vanish_line
             ccs_ru = self.calc_ccs_xy_by_uv(uv_ru)
 
-        ipm_lu = [max(0, int((ccs_lu[0,0]+self.ipm_width_x_3d/2.0)/self.ipm_step_xy)), max(0, self.ipm_image_height-1-int(ccs_lu[1,0]/self.ipm_step_xy))]
-        ipm_ru = [min(self.ipm_image_width-1, int((ccs_ru[0,0]+self.ipm_width_x_3d/2.0)/self.ipm_step_xy)), max(0, self.ipm_image_height-1-int(ccs_ru[1,0]/self.ipm_step_xy))]
-        ipm_lb = [max(0, int((ccs_lb[0,0]+self.ipm_width_x_3d/2.0)/self.ipm_step_xy)), min(self.ipm_image_height-1, self.ipm_image_height-1-int(ccs_lb[1,0]/self.ipm_step_xy))]
-        ipm_rb = [min(self.ipm_image_width-1, int((ccs_rb[0,0]+self.ipm_width_x_3d/2.0)/self.ipm_step_xy)), min(self.ipm_image_height-1, self.ipm_image_height-1-int(ccs_rb[1,0]/self.ipm_step_xy))]
+        ipm_lu = [max(0, int((ccs_lu[0,0]+self.world_width/2.0)/self.world_pixel)), max(0, self.ipm_image_height-1-int(ccs_lu[1,0]/self.world_pixel))]
+        ipm_ru = [min(self.ipm_image_width-1, int((ccs_ru[0,0]+self.world_width/2.0)/self.world_pixel)), max(0, self.ipm_image_height-1-int(ccs_ru[1,0]/self.world_pixel))]
+        ipm_lb = [max(0, int((ccs_lb[0,0]+self.world_width/2.0)/self.world_pixel)), min(self.ipm_image_height-1, self.ipm_image_height-1-int(ccs_lb[1,0]/self.world_pixel))]
+        ipm_rb = [min(self.ipm_image_width-1, int((ccs_rb[0,0]+self.world_width/2.0)/self.world_pixel)), min(self.ipm_image_height-1, self.ipm_image_height-1-int(ccs_rb[1,0]/self.world_pixel))]
 
         uv_list = [uv_lu, uv_ru, uv_lb, uv_rb]
         ipm_list = [ipm_lu, ipm_ru, ipm_lb, ipm_rb]
@@ -109,95 +119,116 @@ class IPM():
         uv_array = np.array(uv_list, dtype=np.float32)
         ipm_array = np.array(ipm_list, dtype=np.float32)
 
-        return uv_array,ipm_array
+        return uv_array, ipm_array
+
+    def get_undistort_map(self):
+        camera_matrix = self.K.astype(np.float32)
+        dist_coeffs = self.distort.astype(np.float32)
+        map1, map2 = cv2.initUndistortRectifyMap(camera_matrix, dist_coeffs, None, camera_matrix, (self.image_width, self.image_height), cv2.CV_32FC1)
+
+        return map1, map2
+
+    def get_point_cloud(self):
+        # perspective image to camera(normalized)
+        vs, us = np.indices((self.image_height, self.image_width))
+        coords = np.column_stack((us.ravel(), vs.ravel(), np.ones(self.image_height * self.image_width)))
+        # coords = np.linalg.inv(self.K) @ coords.transpose()
+        xs = (coords[:, 0] - self.K[0, 2]) / self.K[0, 0]
+        ys = (coords[:, 1] - self.K[1, 2]) / self.K[1, 1]
+        zs = np.ones(self.image_height * self.image_width)
+        coords = np.vstack((xs, ys, zs))
+        
+        # camera to world
+        # coords is normalized(no depth), so can't directly calculate
+        # coords = np.linalg.inv(self.Rwc) @ (coords - self.Twc)
+        Rcw = self.Rwc.transpose() # the inverse of an orthogonal matrix is its transpose 
+        Tcw = Rcw @ (-self.Twc)
+        coords = Rcw @ coords
+        coef = (0 - Tcw[2]) / coords[2, :] # zknown = z * coef + T[2]
+        coords = coords * coef + Tcw
+
+        return coords
 
     def get_ipm_map(self):
         """
-        与onboard 中 c++ IPM相同
+        与onboard中c++IPM相同
         """
-        rc_array = np.zeros((self.ipm_image_height*self.ipm_image_width,2),dtype=np.int32)
-        pt_ccs_array = np.zeros((self.ipm_image_height*self.ipm_image_width,3))   #[w*h, 3]
-        for r, c in self.pair(self.ipm_image_width, self.ipm_image_height):
-            x = -self.ipm_width_x_3d / 2.0 + self.ipm_step_xy * c
-            y = self.ipm_step_xy * (self.ipm_image_height-1-r) + self.ipm_height_y_start
-            z = 0
-            pt_ccs = np.array([x,y,z]).reshape(1,3)
-            pt_ccs_array[r*self.ipm_image_width+c,:] = pt_ccs[0,:]
-            rc_array[r*self.ipm_image_width+c,0] = r
-            rc_array[r*self.ipm_image_width+c,1] = c
+        # ipm image to world
+        vs, us = np.indices((self.ipm_image_height, self.ipm_image_width))
+        coords = np.column_stack((us.ravel(), vs.ravel()))
+        xs = self.world_pixel * coords[:, 0] - self.world_width / 2.0
+        ys = self.world_pixel * (self.ipm_image_height - 1 - coords[:, 1]) + self.world_y_start
+        zs = np.zeros(self.ipm_image_height * self.ipm_image_width)
+        coords = np.vstack((xs, ys, zs))
+        
+        # world to camera
+        coords = self.Rwc @ coords + self.Twc
 
-        pt_ocs_array = pt_ccs_array @ self.R_ccs2ocs.transpose() + self.T_ccs2ocs.reshape(1,3)  #[N,3]
-        pt_ocs_array = (pt_ocs_array / pt_ocs_array[:,2].reshape(-1,1))
-        pt_uv_array = pt_ocs_array @ self.K.transpose()
-        pt_uv_array = (pt_uv_array + 0.5).astype(np.int32)
+        # camera to perspective image
+        coords = coords / coords[2, :]
+        coords = self.K @ coords
 
-        idx = list(np.where((pt_uv_array[:,1]>self.vanish_line) & (pt_uv_array[:,1]<self.raw_H) & (pt_uv_array[:,0]>=0) & (pt_uv_array[:,0]<self.raw_W)))[0]
-        img_uvs = pt_uv_array[idx[:], :2]
-        ipm_rcs = rc_array[idx[:],:]
+        # filter
+        coords = coords.transpose().reshape(self.ipm_image_height, self.ipm_image_width, 3).astype(np.int64)
+        mask = (0 <= coords[:, :, 0]) & (self.image_width > coords[:, :, 0]) \
+                & (self.vanish_line <= coords[:, :, 1]) & (self.image_height > coords[:, :, 1])
 
-        return ipm_rcs,img_uvs
+        return coords, mask
 
-    def undistort(self, img):
-        height = img.shape[0]
-        width  = img.shape[1]
+    # def transform(self):
+    #     for name in sorted(os.listdir(self.image_dir)):
+    #         path = os.path.join(self.image_dir, name)
+    #         print(f"[transform] read perspective image {path}")
 
-        # 相机内参 fx cx fy cy
-        cam_0_matrix = [[2141.9937, 0., 1724.7345],
-                        [0., 2144.1709, 1106.3953],
-                        [0., 0., 1.]]
-        # 去畸变参数k1 k2 p1 p2 k3
-        cam_0_distcoeffs = [-0.3847, 0.1769, -0.000679, -0.000386, -0.0413]
+    #         img = cv2.imread(path, cv2.IMREAD_GRAYSCALE | cv2.IMREAD_COLOR)
+    #         assert((img.shape[0]==self.image_height) and (img.shape[1]==self.image_width))
 
-        camera_matrix = np.array(cam_0_matrix, dtype=np.float32)
+    #         # undistort
+    #         img = cv2.remap(img, self.undistort_map1, self.undistort_map2, interpolation=cv2.INTER_NEAREST)
 
-        distortion = np.array(cam_0_distcoeffs, dtype=np.float32)
-        map1, map2 = cv2.initUndistortRectifyMap(camera_matrix, distortion, None, camera_matrix, (width, height), cv2.CV_32FC1)
-        img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_NEAREST)
-
-        return img
-
-    def get_ipm_images(self):
-        # 1
-        ipm_rcs,img_uvs = self.get_ipm_map()
-
-        # 2
-        # uv_array,ipm_array = self.get_ipm_rect()
-        # M = cv2.getPerspectiveTransform(uv_array, ipm_array)
-
-        for f in self.images_name:
-            img = cv2.imread(os.path.join(self.image_dir, f), cv2.IMREAD_COLOR if 3 == self.raw_C else cv2.IMREAD_GRAYSCALE)
-            img = self.undistort(img)
+    #         # point cloud
+    #         # point_cloud = np.hstack((self.point_cloud.transpose(), img.reshape(-1, 3).astype(np.float64)))
             
-            try:    
-                # 输入图片大小与默认尺寸不匹配报错！提醒：如果图片大小不匹配需要对应修改内参矩阵和消失线
-                assert((img.shape[0]==self.raw_H) and (img.shape[1]==self.raw_W))
-                dst_path = os.path.join(self.output_dir, f)
+    #         # world to ipm image
+    #         us = (self.point_cloud[0, :] + self.world_width / 2.0) / self.world_pixel
+    #         vs = (self.world_height + self.world_y_start - self.point_cloud[1, :]) / self.world_pixel
+    #         coords = np.column_stack((us, vs)).astype(np.int64)
 
-                # 1
-                if 3 == self.raw_C:
-                    img_ipm = np.zeros((self.ipm_image_height, self.ipm_image_width, 3), dtype=np.uint8)
-                else:
-                    img_ipm = np.zeros((self.ipm_image_height, self.ipm_image_width), dtype=np.uint8)
-                img_ipm[ipm_rcs[:,0],ipm_rcs[:,1]] = img[img_uvs[:,1],img_uvs[:,0]]
+    #         # filter
+    #         coords = coords.reshape(img.shape[0], img.shape[1], 2)
+    #         mask = (0 <= coords[:, :, 0]) & (self.ipm_image_width > coords[:, :, 0]) \
+    #                 & (0 <= coords[:, :, 1]) & (self.ipm_image_height > coords[:, :, 1])
+    #         mask[np.arange(img.shape[0]) < self.vanish_line] = False
 
-                # 2
-                # img_ipm = cv2.warpPerspective(img, M, (self.ipm_image_width,self.ipm_image_height))
+    #         # transform 
+    #         img_ipm = np.zeros((self.ipm_image_height, self.ipm_image_width, 3), dtype=np.uint8)
+    #         img_ipm[coords[mask][:, 1], coords[mask][:, 0]] = img[np.argwhere(mask)[:, 0], np.argwhere(mask)[:, 1]]
 
-                if self.resize is not None:
-                    img_ipm = cv2.resize(img_ipm, dsize=tuple(self.resize), interpolation=cv2.INTER_NEAREST)
- 
-                cv2.imwrite(dst_path, img_ipm)
-                print(f"write image {dst_path}")
+    #         # save ipm image
+    #         if self.resize is not None:
+    #             img_ipm = cv2.resize(img_ipm, dsize=tuple(self.resize), interpolation=cv2.INTER_NEAREST)
+    #         path = os.path.join(self.output_dir, name)
+    #         cv2.imwrite(path, img_ipm)
+    #         print(f"[transform] save ipm image {path}")
 
-                # # 原图与结果图对比
-                # if 3 == self.raw_C:
-                #     img_merge = np.zeros((img.shape[0] + img_ipm.shape[0], img.shape[1] + img_ipm.shape[1], 3), dtype=np.uint8)
-                # else:
-                #     img_merge = np.zeros((img.shape[0] + img_ipm.shape[0], img.shape[1] + img_ipm.shape[1]), dtype=np.uint8)
-                # img_merge[0:img.shape[0], 0:img.shape[1]] = img
-                # img_merge[0:img_ipm.shape[0], img.shape[1]:img_merge.shape[1]] = img_ipm
-                # merge_path = os.path.join(self.merge_dir, f)
-                # cv2.imwrite(merge_path, img_merge)
-                # print(f"write compare image {merge_path}")
-            except:
-                print(f"Warning: image {os.path.join(self.image_dir, f)} size not match!")
+    def transform(self):
+        for name in sorted(os.listdir(self.image_dir)):
+            path = os.path.join(self.image_dir, name)
+            print(f"[transform] read perspective image {path}")
+
+            img = cv2.imread(path, cv2.IMREAD_GRAYSCALE | cv2.IMREAD_COLOR)
+            assert((img.shape[0]==self.image_height) and (img.shape[1]==self.image_width))
+
+            # undistort
+            img = cv2.remap(img, self.undistort_map1, self.undistort_map2, interpolation=cv2.INTER_NEAREST)
+
+            # transform
+            img_ipm = np.zeros((self.ipm_image_height, self.ipm_image_width, 3), dtype=np.uint8)
+            img_ipm[np.argwhere(self.mask)[:, 0], np.argwhere(self.mask)[:, 1]] = img[self.map[self.mask][:, 1], self.map[self.mask][:, 0]]
+
+            # save ipm image
+            if self.resize is not None:
+                img_ipm = cv2.resize(img_ipm, dsize=tuple(self.resize), interpolation=cv2.INTER_NEAREST)
+            path = os.path.join(self.output_dir, name)
+            cv2.imwrite(path, img_ipm)
+            print(f"[transform] save ipm image {path}")
--- ai/IPM/main.py
+++ ai/IPM/main.py
@@ -1,95 +1,77 @@
+import os
 import yaml
-import ipm
+
+from ipm import IPM
+
 
 def config_parser():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str,  default='./config_3840x2160.yaml', help='config file')
-    parser.add_argument("--image_dir", type=str, default='./data/images', help='image directiory')
-    parser.add_argument("--output_dir", type=str, default='./output', help='output directiory')
-    parser.add_argument("--resize", nargs='+', type=int, help='image resize scale for save after IPM')
-    # parser.add_argument("--width", type=int, default=960, help='image width')
-    # parser.add_argument("--height", type=int, default=540, help='image height')
-    # parser.add_argument("--channel", type=int, default=1, help='image channel')
-    parser.add_argument("--width", type=int, default=3840, help='image width')
-    parser.add_argument("--height", type=int, default=2160, help='image height')
-    parser.add_argument("--channel", type=int, default=3, help='image channel')
-    # parser.add_argument("--vanish_line", type=int, default=280, help='vanish line row')
-    parser.add_argument("--vanish_line", type=int, default=1120, help='vanish line row')
-    parser.add_argument("--ipm_width_x_3d", type=float, default=30.0, help='ipm range 3d x, unit:m')
-    parser.add_argument("--ipm_height_y_3d", type=float, default=8.0, help='ipm range 3d y, unit:m')
-    # parser.add_argument("--ipm_step_xy", type=float, default=0.04, help='ipm step 3d, unit:m')
-    parser.add_argument("--ipm_step_xy", type=float, default=0.01, help='ipm step 3d, unit:m')
-    parser.add_argument("--ipm_height_y_start", type=float, default=3.0, help='ipm start 3d y, unit:m')
-    parser.add_argument("--intrinsic", nargs='+', type=float, help='camera intrinsic parameters fx,fy,cx,cy')
-    parser.add_argument("--R_ccs2ocs", nargs='+', type=float, help='rotation matrix from ccs to ocs')
-    parser.add_argument("--T_ccs2ocs", nargs='+', type=float, help='translation vector from ccs to ocs')
+    parser.add_argument("--config", type=str,  default="config_csmp_960x540.yaml", help="config file")
+    parser.add_argument("--image_dir", type=str, help="image directiory")
+    parser.add_argument("--output_dir", type=str, help="output directiory")
+    parser.add_argument("--image_width", type=int, help="image width")
+    parser.add_argument("--image_height", type=int, help="image height")
+    parser.add_argument("--resize", nargs="+", type=int, help="image resize scale for save after IPM")
+    parser.add_argument("--vanish_line", type=int, help="vanish line row")
+    parser.add_argument("--world_width", type=float, help="world width, unit:m")
+    parser.add_argument("--world_height", type=float, help="world height, unit:m")
+    parser.add_argument("--world_pixel", type=float, help="world pixel, unit:m")
+    parser.add_argument("--world_y_start", type=float, help="world y start, unit:m")
+    parser.add_argument("--intrinsic", nargs="+", type=float, help="camera intrinsic parameters fx,fy,cx,cy")
+    parser.add_argument("--Rwc", nargs="+", type=float, help="rotation matrix from ccs to ocs")
+    parser.add_argument("--Twc", nargs="+", type=float, help="translation vector from ccs to ocs")
 
     return parser
+
+
+def parser_config(name):
+    import yaml
+
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(cwd, "config", name)) as file:
+        cfg = yaml.safe_load(file)
+
+    return cfg
+
 
 def main():
     parser = config_parser()
     args = parser.parse_args()
+    cfg = parser_config(args.config)
 
-    with open(args.config, 'r') as f:
-        cfg = yaml.load(f, Loader=yaml.FullLoader)
-    print(f"{args.config}:\n{cfg}")
+    if args.image_dir is not None:
+        cfg["image_dir"] = args.image_dir
+    if args.output_dir is not None:
+        cfg["output_dir"] = args.output_dir
+    if args.image_width is not None:
+        cfg["image_width"] = args.image_width
+    if args.image_height is not None:
+        cfg["image_height"] = args.image_height
+    if args.resize is not None:
+        cfg["resize"] = args.resize
+    if args.vanish_line is not None:
+        cfg["vanish_line"] = args.vanish_line
+    if args.world_width is not None:
+        cfg["world_width"] = args.world_width
+    if args.world_height is not None:
+        cfg["world_height"] = args.world_height
+    if args.world_pixel is not None:
+        cfg["world_pixel"] = args.world_pixel
+    if args.world_y_start is not None:
+        cfg["world_y_start"] = args.world_y_start
+    if args.intrinsic is not None:
+        cfg["intrinsic"] = args.intrinsic
+    if args.Rwc is not None:
+        cfg["Rwc"] = args.Rwc
+    if args.Twc is not None:
+        cfg["Twc"] = args.Twc
+    print(f"[main] {args.config} >> \n{cfg}")
 
-    if cfg.get('image_dir') is not None:
-        args.image_dir = cfg['image_dir']
-    if cfg.get('output_dir') is not None:
-        args.output_dir = cfg['output_dir']
-    if cfg.get('width') is not None:
-        args.width = cfg['width']
-    if cfg.get('height') is not None:
-        args.height = cfg['height']
-    if cfg.get('channel') is not None:
-        args.channel = cfg['channel']
-    if cfg.get('resize') is not None:
-        args.resize = cfg['resize']
-    if cfg.get('vanish_line') is not None:
-        args.vanish_line = cfg['vanish_line']
-    if cfg.get('ipm_width_x_3d') is not None:
-        args.ipm_width_x_3d = cfg['ipm_width_x_3d']
-    if cfg.get('ipm_height_y_3d') is not None:
-        args.ipm_height_y_3d = cfg['ipm_height_y_3d']
-    if cfg.get('ipm_step_xy') is not None:
-        args.ipm_step_xy = cfg['ipm_step_xy']
-    if cfg.get('ipm_height_y_start') is not None:
-        args.ipm_height_y_start = cfg['ipm_height_y_start']
-    if cfg.get('intrinsic') is not None:
-        args.intrinsic = cfg['intrinsic']
-    if cfg.get('R_ccs2ocs') is not None:
-        args.R_ccs2ocs = cfg['R_ccs2ocs']
-    if cfg.get('T_ccs2ocs') is not None:
-        args.T_ccs2ocs = cfg['T_ccs2ocs']
+    ipm = IPM(cfg)
+    ipm.transform()
 
-    # # 960x540
-    # if args.intrinsic is None:
-    #     args.intrinsic = [476.2483, 477.0686, 484.8964, 262.8846]
-    # if args.R_ccs2ocs is None:
-    #     args.R_ccs2ocs = [0.9937560081539198, 0.1109831719351175, 0.01147744767823091, 
-    #                       0.003276455792956701, 0.07379581423704455, -0.9972679893782455, 
-    #                       -0.111526952327333, 0.9910786615341447, 0.07297140231806608]
-    # if args.T_ccs2ocs is None:
-    #     args.T_ccs2ocs = [0.2317971025971608, 1.713913045205863, 1.913995241236965]
-    
-    # 3840x2160
-    if args.intrinsic is None:
-        args.intrinsic = [2141.9937, 2144.1709, 1724.7345, 1106.3953]
-    if args.R_ccs2ocs is None:
-        args.R_ccs2ocs = [0.999616, -0.00817016, 0.0264734,
-                          0.0263923, -0.00988875, -0.999603,
-                          0.0084287, 0.999918, -0.00966933]
-    if args.T_ccs2ocs is None:
-        args.T_ccs2ocs = [-0.498692, 1.619, 2.51533]
-
-    # 找到IPM图像上对应到原始图像上的四个边界点
-    model = ipm.IPM(args)
-    model.get_ipm_images()
-
-    return
 
 if __name__=="__main__":
     main()
